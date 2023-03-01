44cb2f75373d0921c8cd8773b5c26944c5f9e672
Azath <azathabdulsamadh@ufl.edu>
2023-03-01 11:51:02 -0500

    Added comments to board.py

--- board.py
+++ board.py
@@ -16,44 +16,61 @@ class Board:
     def __init__(self, root, mode):
         self.root = root
         self.mode = mode
+        # Recognition mode will recognize user inputs
         if self.mode == 'recognition':
+            
+            # Create canvas, clear button and label to show predicted gesture, confidence and time taken to predict
             self.createCanvas()
             self.createClearButton()
             self.createPredictionLabels()
             self.points = []
+
+            # Invoking the recognizer module
             self.recognizer = Recognizer()
             self.startPointX = 0
             self.startPointY = 0
+        # Collection mode will only store user input as it is
         elif self.mode == 'collection':
+
+            # Current working directory will be used to load gesture image as a reference
             self.currentWorkingDirectory = os.getcwd()
-            print(self.currentWorkingDirectory)
+            
+            # Flags which will indicate the user added status and collection start status
             self.userAdded = False
             self.readyToStore = False
+            
+            # Creating variables to track current user parameters
             self.gestureList = GESTURE_LIST
             self.currentUser = ''
             self.userDrawCount = 0
             self.gestureIndex = 0
-            self.currentUserId = 'sampleUser'
-            self.currentGesture = 'sampleGesture'
+            self.currentUserId = ''
+            self.currentGesture = ''
             self.points = []
             self.startPointX = 0
             self.startPointY = 0
+
+            # Create canvas for drawing
             self.createCanvas()
+
+            # Create clear button to enable user to clear the canvas
             self.createClearButton()
-            # # To be added
-            # 1. DB module to store user points with user id in a json - Done
+
+            # Invoking DB module to store user points with user id in a json
             self.db = Database()
-            # 2. Show sample drawing on top right
-            # 3. Add button to submit user input - Done
+
+            # Added submit button to enable user to submit their drawing
             self.createSubmitButton()
-            # 4. Add label to show prompt to be drawn - Done
+
+            # Label to prompt the user
             self.createPromptLabel()
-            self.setPromptLabel('Please enter userId',1)
-            # 5. Add logic to show prompt and store points - Inprogress
-            # 6. Add text box to get user ID and any other user data
+            self.setPromptLabel('Please Enter User ID!',1)
+            
+            # Text box to get user ID
             self.createUserIdTextBox()
+
+            # Label to show reference gesture image to user
             self.createGestureImageLabel()
-            # 7. Convert json(database.json) to xml
     
     # def collectFromUser(self, userId):
     #     # Delete any existing user with same userId in DB and start fresh
@@ -64,28 +81,35 @@ class Board:
     #             self.setPromptLabel('Please draw a {}'.format(gesture))
 
     
+    # Function to create the canvas - drawing board
+    # Creates the drawing board and sets mouse bindings to track user click and drag movements
     def createCanvas(self):
         self.board = Canvas(self.root, width=BOARD_WIDTH, height=BOARD_HEIGHT, bg=BOARD_BG)
         self.setMouseBindings()
         self.board.pack()
 
+    # Function to create user ID Text box to get user ID from the user
     def createUserIdTextBox(self):
         self.userIdTextBox = Text(self.root, width=TEXT_BOX_WITDH, height=TEXT_BOX_HEIGHT)
         self.userIdTextBox.pack(side='top')
 
+    # Function to create clear button to enable user to clear the drawing board
     def createClearButton(self):
         self.clearButton = Button(self.root, text=CLEAR_BUTTON_TEXT)
         self.clearButton.configure(command=self.onClearButtonClick)
         self.clearButton.pack(side = 'left')
 
+    # Function to create a submit button to enable user to submit their drawing
     def createSubmitButton(self):
         self.submitButton = Button(self.root, text=SUBMIT_BUTTON_TEXT)
         self.submitButton.configure(command=self.onSubmitButtonClick)
         self.submitButton.pack(side = 'left')
     
+    # Function to create lable to show user a reference of the gesture to be drawn
     def createGestureImageLabel(self):
         self.gestureImageLabel = Label(self.root)
 
+    # Function to create labels to show predicted gesture, confidence and time taken to predict
     def createPredictionLabels(self):
         self.predictedGestureLabel = Label(self.root)
         self.confidenceLabel = Label(self.root)
@@ -95,19 +119,23 @@ class Board:
         self.confidenceLabel.pack()
         self.timelabel.pack()
     
+    # Function to set image to show the user as a reference
     def setGestureImageLabel(self, img):
         self.gestureImageLabel.configure(image = img)
         self.gestureImageLabel.image = img
         self.gestureImageLabel.pack(side='right')
 
+    # Function to clear the gesture reference image when user has finished drawing
     def clearGestureImageLabel(self):
         self.gestureImageLabel.destroy()
     
+    # Function to set values to prediction labels
     def setPredictionLabels(self, recognizedGesture, score, time):
         self.predictedGestureLabel.configure(text="Predicted Gesture = "  + str(recognizedGesture))
         self.confidenceLabel.configure(text="Confidence = "  + str(round(score,2))) 
         self.timelabel.configure(text="Time = "  + str(round(time*1000,2)) + " ms" )
 
+    # Function to clear prediction labels
     def clearPredictionLables(self):
         self.predictedGestureLabel.configure(text="")
         self.timelabel.configure(text="")
@@ -144,38 +172,57 @@ class Board:
         print(LOG_BOARD_CLEARED)
     
     def onSubmitButtonClick(self):
+    # Check if a user has been added
         if not self.userAdded:
+            # Get the user ID from a text box
             userId = self.userIdTextBox.get(1.0, "end-1c")
+            # Set the current user and add them to the database
             self.currentUser = userId
             self.db.addUser(userId)
+            # Display a welcome message to the user
             self.setPromptLabel('Welcome {}!'.format(userId), 1)
+            # Set userAdded to True to indicate that a user has been added
             self.userAdded = True
+        
+        # Check if the system is ready to store the gesture drawing
         if self.readyToStore:
+            # Get the index of the current gesture and add the gesture drawing to the database
             gestureIndex = (self.gestureIndex - 1)%len(self.gestureList)
             self.db.addGesture(self.currentUser, self.gestureList[gestureIndex], deepcopy(self.points))
+            # Clear the drawing board and points list
             self.points.clear()
             self.board.delete(BOARD_DELETE_MODE)
+        
+        # Check if the user has completed all their drawings
         if self.userDrawCount < 5:
+            # Get the name of the current gesture and prompt the user to draw it
             gestureName = self.gestureList[self.gestureIndex]
             self.setPromptLabel('Please draw a {}'.format(gestureName), 2)
+            # Display an image of the gesture
             self.setGestureImageLabel(self.loadImage(gestureName))
+            # Update the userDrawCount and gestureIndex variables
             self.userDrawCount += 1
             self.gestureIndex = (self.gestureIndex + 1)%len(self.gestureList)
+            # Set readyToStore to True to indicate that the system is ready to store the next drawing
             self.readyToStore = True
         else:
+            # If the user has completed all the drawings, display a message and clear the gesture image label
             self.setPromptLabel('Saving your contribution!', 1)
             self.setPromptLabel('Thank you for participating, {}!'.format(self.currentUser), 2)
             self.clearGestureImageLabel()
             self.root.update()
             sleep(2)
             self.setPromptLabel('', 2)
+            # Create an XML file with the collected user logs, reset userDrawCount and gestureIndex, and display a prompt to start again
             self.createXMLUserLogs()
             self.userDrawCount = 0
             self.gestureIndex = 0
             self.setPromptLabel('Please enter user ID and click Submit to Start!', 1)
+            # Set userAdded and readyToStore to False to indicate that a new user needs to be added and the system is not ready to store a drawing yet
             self.userAdded = False
             self.readyToStore = False
 
+    # Function to return last coordinates of the mouse click
     def getLastCoordinates(self,event):
         self.startPointX,self.startPointY=event.x,event.y
 
@@ -216,43 +263,64 @@ class Board:
         print(LOG_DRAWING_FINISHED)
     
     def createXMLUserLogs(self):
+        # Open the JSON file containing the user data and load it into a dictionary
         file = open('database.json')
         user_data = json.load(file)
         file.close()
-        
+
+        # Get the current working directory
         root = os.getcwd()
+
+        # Define the name and path of the directory where the XML logs will be saved
         log_directory_name = 'xml_collected_logs'
         log_directory_path = os.path.join(root, log_directory_name)
+
+        # If the directory already exists, delete it and create a new one
         if os.path.isdir(log_directory_path):
             rmtree(log_directory_path)
         os.makedirs(log_directory_path)
 
+        # Iterate over the users in the user data dictionary
         for user in user_data:
-            # print(user)
+
+            # Create a directory for the user in the XML logs directory
             user_path = os.path.join(log_directory_path, user)
             os.makedirs(user_path)
+
+            # Iterate over the gestures of the user
             for gesture in user_data[user]:
-                # print(gesture)
-                # print(len(user_data[user][gesture]))
+
+                # Iterate over each set of points for the gesture
                 for i in range(0,len(user_data[user][gesture])):
+
+                    # Get the list of points for the gesture
                     pointList = user_data[user][gesture][i]
-                    # print(user, gesture, len(pointList))
+
+                    # Create a new XML document
                     root = minidom.Document()
                     gestureChild = root.createElement('Gesture')
+
+                    # Set the attributes of the gesture element
                     gestureChild.setAttribute('User', str(user))
                     gestureChild.setAttribute('Gesture', '{}{}'.format(gesture,i+1))
                     gestureChild.setAttribute('Number', str(i+1))
                     gestureChild.setAttribute('NumPts', str(len(pointList)))
                     gestureChild.setAttribute('Date', strftime("%d-%m-%Y"))
                     gestureChild.setAttribute('Time', strftime("%H:%M:%S"))
+
+                    # Append the gesture element to the root element
                     root.appendChild(gestureChild)
+
+                    # Iterate over the points of the gesture and create a new point element for each point
                     for point in pointList:
                         pointChild = root.createElement('Point')
                         pointChild.setAttribute('X', str(point[0]))
                         pointChild.setAttribute('Y', str(point[1]))
                         gestureChild.appendChild(pointChild)
-                        gestureRootString = root.toprettyxml(indent= "\t")
-                        file_name = '{}{}.xml'.format(gesture,i+1)
-                        file_path = os.path.join(user_path, file_name)
-                        with open(file_path, "w") as file:
-                            file.write(gestureRootString) 
+
+                    # Get the string representation of the XML document and write it to a file
+                    gestureRootString = root.toprettyxml(indent= "\t")
+                    file_name = '{}{}.xml'.format(gesture,i+1)
+                    file_path = os.path.join(user_path, file_name)
+                    with open(file_path, "w") as file:
+                        file.write(gestureRootString)
